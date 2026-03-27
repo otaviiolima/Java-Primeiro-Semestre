8b0501fe4a6ad4c079edcb58b91e98920c8cc2d3
Luiz Otávio Lima <otaviio.lima@hotmail.com>
2026-03-27 10:04:36 -0300

    Enhance code readability with comments
    Added comments to clarify user input and logic.

--- Questão1Atividade1.java
+++ Questão1Atividade1.java
@@ -4,7 +4,7 @@ public class Questão1Atividade1 {
     static void main() {
         Scanner scanner = new Scanner(System.in);
 
-        int a,b,c;
+        // O usuário vai digitar os três números.
 
         System.out.println("Digite o primeiro número: ");
         int numA = scanner.nextInt();
@@ -14,6 +14,8 @@ public class Questão1Atividade1 {
 
         System.out.println("Digite o terceiro número: ");
         int numC = scanner.nextInt();
+
+        // Aqui o programa vai somar o primeiro (numA) e segundo número (numB) e vai dizer se é maior ou menor que o terceiro número (numC).
 
         if (numA + numB > numC) {
             System.out.println("A soma entre os dois números é maior que C!");
